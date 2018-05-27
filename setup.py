83dc03d5b958ecde658a23bbb0ca225492cc1dd8
Jan Helbling <jan.helbling@gmail.com>
2018-05-27 18:56:19 +0200

    Better setup

--- setup.py
+++ setup.py
@@ -8,8 +8,5 @@ setup(name='pyFormgrabber',
 	author='Jan Helbling',
 	author_email='jan.helbling@gmail.com',
 	url='http://www.jan-helbling.ch/~jhelbling/linux.py?gpl3-opensource-library=pyFormgrabber-grab-html-forms-in-python',
-	package_dir = {'pyFormgrabber' : 'lib'},
-	packages=['pyFormgrabber'],
+	py_modules=['pyFormgrabber'],
 )
-
-print("On ubuntu you need to add /usr/ (local) /lib/python3.4/site-packages/ to your PYTHONPATH environmentvariable")
